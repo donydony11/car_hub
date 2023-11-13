6b0ff6c97ec361892a93a30bb7699b037ec83422
donydony11 <121674533+donydony11@users.noreply.github.com>
2023-11-13 17:41:20 +0200

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from '@/components/Hero'
+import CustomButton from './CustomButton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+}
